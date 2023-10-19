41fc4581cfdb9cd05e1c3aaff946b9af49ff5058
“Dimitri” <dimitri.s.amado@gmail.com>
2023-10-19 00:07:25 -0300

    Timers exercise

--- Timers/chronometer.ino
+++ Timers/chronometer.ino
@@ -1,20 +1,23 @@
 /*
-    Crie um projeto que após a inicialização do ATMega328p, um LED Vermelho
-    fique piscando com um tempo de 150ms. O projeto deve fazer uso do periférico
-    TIMER e não pode conter linhas de delay. 
+    Desenvolva um programa que faça com que um LED Vermelho indique cada segundo
+    (fique aceso por 100ms e apaga) e um LED Verde indique os minutos (acende por
+    100ms e apaga). Use o temporizador
 */
 
-#define LED (1 << PD7)
+#define LED1 (1 << PD7)
+#define LED2 (1 << PB0)
 
-unsigned int cont = 0;
+unsigned int cont1 = 0;
+unsigned int cont2 = 0;
 
 int main(){
   // Configurando o LED como saída
-  DDRD = LED;
+  DDRD = LED1;
+  DDRB = LED2;
 
   // Configurando temporizador - Modo Comparador
   TCCR0A = (1<<WGM01); //Configuração do modo de funcionamento para comparador
-
+  
   // Configurando o Pre-scaler
   TCCR0B = (1<<CS01); //Pre-scaler de 8 (frequencia de 2MHz - periodo de 500ns em cada contagem)
 
@@ -35,12 +38,25 @@ int main(){
 
 // o q acontece a cada 100μs ???
 ISR(TIMER0_COMPA_vect){
-  cont++;
+  cont1++;
+  cont2++;
   
-  // Quando passar 150ms
-  if(cont == 1500)
-  {
-  	cont = 0;
-    PORTD ^= LED;
+  // Quando passar 100ms
+  if(cont1 == 10000){
+    PORTD ^= LED1;
+  }
+
+  else if(cont1 == 11000){
+    cont1 = 0;
+    PORTD ^= LED1;
+  }
+
+  if(cont2 == 600000) {
+    PORTB ^= LED2;
+  }
+
+  else if(cont2 == 601000){
+    cont2 = 0;
+    PORTB ^= LED2;
   }
 }

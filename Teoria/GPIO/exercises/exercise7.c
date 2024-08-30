6dc5b00c434e324bda28680c421ae4848a4cc80a
“Dimitri” <dimitri.s.amado@gmail.com>
2024-08-30 00:21:11 -0300

    :sparkles: feat: GPIO Exercise List

--- Teoria/GPIO/exercises/exercise7.c
+++ Teoria/GPIO/exercises/exercise7.c
@@ -1,9 +1,8 @@
 /*
     Escreva um programa que configure o pino PD2 como entrada. Use a operação 
     de AND (&) para verificar se o bit correspondente a esse pino está setado no
-    registro PIN e, caso positivo, acenda o LED no pino PD3 usando a operação de OR (|).
-
-    8 - Modifique no programa anterior para apagar o LED no pino PD3 usando a operação de AND com NOT (&~) se o pino PD2 estiver setado.
+    registro PIN e, caso positivo, acenda o LED no pino PD3 usando a operação de
+    OR (|).
 */
 
 #include <avr/io.h>
@@ -17,8 +16,6 @@ int main(void) {
     while (1) {
         if (!(PIND & BUTTON != 0)) {
             PORTD |= LED;
-        } else {
-            PORTD &= ~LED;
         }
     }
 }
---
+++ Teoria/GPIO/exercises/exercise8.c
@@ -0,0 +1,21 @@
+/*
+    Modifique no programa anterior para apagar o LED no pino PD3 usando a
+    operação de AND com NOT (&~) se o pino PD2 estiver setado.
+*/
+
+#include <avr/io.h>
+
+#define BUTTON (1 << PD2)
+#define LED (1 << PD3)
+
+int main(void) {
+    DDRD &= ~BUTTON; // Define o pino como entrada
+
+    while (1) {
+        if (!(PIND & BUTTON != 0)) {
+            PORTD |= LED;
+        } else {
+            PORTD &= ~LED;
+        }
+    }
+}
